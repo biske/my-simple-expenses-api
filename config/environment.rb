e9bf4dab0a958df304195c13f4f12af40509dc9a
Ivan Bisevac <ivan.bisevac@gmail.com>
2024-02-04 00:33:08 +0100

    Add debug lines to see what is wrong with secret_key_base

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+puts "ENV['RAILS_MASTER_KEY']: #{ENV['RAILS_MASTER_KEY']}"
+puts "ENV['SECRET_KEY_BASE]: #{ENV['SECRET_KEY_BASE']}"
+
 # Initialize the Rails application.
 Rails.application.initialize!
