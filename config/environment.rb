0e9ede5cff2ef0f24e5a465cf6440f71792f6762
Ivan Bisevac <ivan.bisevac@gmail.com>
2024-02-05 10:42:25 +0100

    Put debug lines for port and env

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ require_relative "application"
 
 puts "ENV['RAILS_MASTER_KEY']: #{ENV['RAILS_MASTER_KEY']}"
 puts "ENV['SECRET_KEY_BASE]: #{ENV['SECRET_KEY_BASE']}"
+puts "ENV['RAILS_ENV']: #{ENV['RAILS_ENV']}"
+puts "ENV['PORT']: #{ENV['PORT']}"
 
 # Initialize the Rails application.
 Rails.application.initialize!
