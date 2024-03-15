f9ac2339a1b32a2392cbd6efc8bfbd0cfb5c0982
Ivan Bisevac <ivan.bisevac@gmail.com>
2024-03-15 04:51:11 +0300

    Remove protocol from the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://my-simple-expenses-ui.vercel.app/"
+    origins "my-simple-expenses-ui.vercel.app"
 
     resource "*",
       headers: :any,
